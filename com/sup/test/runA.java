6ba3a3112a7efeb7ee294e03bc1ace138c2c0d6a
luli1015 <luli1015@foxmail.com>
2024-04-14 15:39:08 +0800

    test update6

--- com/sup/test/runA.java
+++ com/sup/test/runA.java
@@ -8,5 +8,6 @@ public class runA {
         System.out.println("Hello World");
         System.out.println("Hello WorlFFFdFFFF");
         System.out.println("Hello BBB");
+        System.out.println("Hello CCC");
     }
 }
